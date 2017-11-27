6fc1385598c25334a1361e8182a6ee47ead65f4a
Alex Lloyd <aplloyd94@gmail.com>
2017-11-27 23:27:50 +0000

    Stupid messy logging config

--- pointy/setup_logging.py
+++ pointy/setup_logging.py
@@ -3,10 +3,22 @@ import os
 
 import yaml
 
+file_handler = {"infofilehandler":
+    {
+        "class": "logging.handlers.RotatingFileHandler",
+        "level": "DEBUG",
+        "formatter": "screen",
+        "filename": "logs // info.log",
+        "maxBytes": 10485760,  # 10MB
+        "backupCount": 20,
+        "encoding": "utf8"
+    }}
+
 
 def setup_logging():
     with open('logging.yaml', 'rt') as log_file:
         config = yaml.safe_load(log_file.read())
     if os.name == 'nt':
+        config['handlers'].append(file_handler)
         config['root']['handlers'].append('infofilehandler')
     logging.config.dictConfig(config)
