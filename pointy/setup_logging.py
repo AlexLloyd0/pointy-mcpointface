7e0513ef0eeb7321b5e9fd41e416e7daea58746c
AlexLloyd1 <aplloyd94@gmail.com>
2017-12-04 11:10:17 +0000

    Remove file logging handler

--- pointy/setup_logging.py
+++ pointy/setup_logging.py
@@ -6,19 +6,9 @@ config = {'disable_existing_loggers': False,
           'handlers': {'console': {'class': 'logging.StreamHandler',
                                    'formatter': 'screen',
                                    'level': 'DEBUG',
-                                   'stream': 'ext://sys.stdout'},
-                       "infofilehandler":
-                           {
-                               "class": "logging.handlers.RotatingFileHandler",
-                               "level": "DEBUG",
-                               "formatter": "screen",
-                               "filename": "logs//info.log",
-                               "maxBytes": 10485760,  # 10MB
-                               "backupCount": 20,
-                               "encoding": "utf8"
-                           }
+                                   'stream': 'ext://sys.stdout'}
                        },
-          'root': {'handlers': ['console', 'infofilehandler'],
+          'root': {'handlers': ['console'],
                    'level': 'DEBUG'},
           'version': 1}
 
